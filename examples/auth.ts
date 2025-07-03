01bb8f9e05979d2cf14188ba3179f7e42ee1dd1f
Tai Vo <tai.a.vo@gmail.com>
2025-07-03 04:44:29 -0700

    add comments

--- examples/auth.ts
+++ examples/auth.ts
@@ -10,6 +10,7 @@ export const auth = betterAuth({
 		},
 	},
 	database: remultAdapter(new InMemoryDataProvider(), {
+		// no need to declare entities when we're generating them
 		authEntities: {},
 	}),
 })
