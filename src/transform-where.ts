8853f0890a0ec1eca670df85176ef13d4c25c459
Tai Vo <tai.a.vo@gmail.com>
2026-03-31 16:15:02 -0700

    wip: redoing transformWhere

--- src/transform-where.ts
+++ src/transform-where.ts
@@ -2,34 +2,51 @@ import type { CleanedWhere } from "better-auth/adapters"
 import { RemultBetterAuthError } from "./utils"
 
 export function transformWhereClause(where: CleanedWhere[] = []) {
-	const entries = where.map((w) => {
-		// if (w.connector === "AND") {
-		// 	const [opKey, opValue] = convertWhereOp(w)
-		// 	return ["$and", { [opKey]: opValue }]
-		// }
-		if (w.connector === "AND") {
-			return transformWhereOp(w)
-		}
+	const conditions = {
+		and: [] as [string, unknown][],
+		or: [] as [string, unknown][]
+	}
 
+	where.forEach(w => {
 		if (w.connector === "OR") {
-			// This situation does not show up in adapter tests. Just log it if it comes up to see
-			// realistic data points
-			console.warn("OR", w)
-			const [opKey, opValue] = transformWhereOp(w)
-			return ["$or", { [opKey]: opValue }]
+			conditions.or.push(transformWhereOp(w))
+		} else {
+			conditions.and.push(transformWhereOp(w))
 		}
-
-		if (w.operator) {
-			// This situation does not show up in adapter tests. Just log it if it comes up to see
-			// realistic data points
-			console.warn("Where with op only", w)
-			return transformWhereOp(w)
-		}
-
-		throw new RemultBetterAuthError(`Unimplemented scenario for where clause: ${JSON.stringify(w)}`)
 	})
 
-	return Object.fromEntries(entries as [string, unknown][])
+	const whereClause = Object.fromEntries(conditions.or.length ? [["$or", conditions.or.map((cond) => ({ [cond[0]]: cond[1] }))]] : conditions.and as [string, unknown][])
+	console.warn("transformWhereClause", where, "whereClause", whereClause)
+	return whereClause
+
+	// const entries = where.map((w) => {
+	// 	if (w.connector === "AND") {
+	// 		return transformWhereOp(w)
+	// 	}
+
+	// 	if (w.connector === "OR") {
+	// 		// This situation does not show up in adapter tests. Just log it if it comes up to see
+	// 		// realistic data points
+	// 		//console.warn("OR", w)
+	// 		const [opKey, opValue] = transformWhereOp(w)
+	// 		const convertedW = ["$or", [{ [opKey]: opValue }]]
+	// 		//console.warn("transformWhereClause", where, "w", w, "convertedW", convertedW)
+	// 		return convertedW
+	// 	}
+
+	// 	if (w.operator) {
+	// 		// This situation does not show up in adapter tests. Just log it if it comes up to see
+	// 		// realistic data points
+	// 		console.warn("Where with op only", w)
+	// 		return transformWhereOp(w)
+	// 	}
+
+	// 	throw new RemultBetterAuthError(`Unimplemented scenario for where clause: ${JSON.stringify(w)}`)
+	// })
+	//
+	// const whereClause = Object.fromEntries(entries as [string, unknown][])
+	// console.warn("transformWhereClause", where, "whereClause", whereClause)
+	// return whereClause
 }
 
 function transformWhereOp({
